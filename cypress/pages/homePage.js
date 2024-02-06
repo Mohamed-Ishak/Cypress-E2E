eb7c601814ad16f14daebad6c612a6ce79ddea06
Mohamed Ishak <mohamed.abdelrahman.ishak@gmail.com>
2024-02-06 11:51:05 +0200

    fix locator

--- cypress/pages/homePage.js
+++ cypress/pages/homePage.js
@@ -1,7 +1,7 @@
 export class HomePage{
     webLocators ={
         searchInput:'[name="search"]',
-        clickSearch:'.btn.btn-default.btn-lg1',
+        clickSearch:'.btn.btn-default.btn-lg',
         product:'img[title="MacBook"]',
         addToCart:'Add to Cart',
         successfullMessage:'.alert.alert-success.alert-dismissible'
